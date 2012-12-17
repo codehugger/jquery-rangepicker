dc7217749db2d4f0eaad4e1813a0e93e8571f421
Bjarki Gudlaugsson <bjarki@codehuggers.com>
2012-12-17 11:54:01 +0000

    strftime knockout binding and some cleanup

--- jquery-rangepicker-knockout.js
+++ jquery-rangepicker-knockout.js
@@ -14,3 +14,21 @@ ko.bindingHandlers.rangepicker = {
         });
     }
 };
+
+ko.bindingHandlers.strftime = {
+    update: function(element, valueAccessor, allBindingsAccessor) {
+        // First get the latest data that we're bound to
+        var value = valueAccessor(), allBindings = allBindingsAccessor();
+
+        // Next, whether or not the supplied model property is observable, get its current value
+        var valueUnwrapped = ko.utils.unwrapObservable(value);
+
+        // Extract the date format from the settings or apply the default which is ISO
+        var dateFormat = allBindings.dateFormat || "%Y-%m-%dT%H:%M:%S";
+
+        // Now manipulate the DOM element
+        if (valueUnwrapped) {
+            $(element).html(valueUnwrapped.strftime(dateFormat));
+        }
+    }
+};
